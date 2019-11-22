b5da91acf510857aefbb58d549f54638b14b6574
Emanuel da Anunciação Silva <emanuel.silva@solutis.com.br>
2019-11-21 23:13:47 -0300

    Incluíndo o security na api.

---
+++ src/main/java/br/com/eps/pontointeligente/api/utils/PasswordUtils.java
@@ -0,0 +1,28 @@
+package br.com.eps.pontointeligente.api.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtils {
+	
+	public static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+	
+	public PasswordUtils() {}
+	
+	public static String generateBCripty(String password) {
+		
+		if(password == null) {
+			return password;
+		}
+		
+		log.info("Gerando hash com o BCrypt.");
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+		
+		
+		return bCryptEncoder.encode(password);
+	}
+	
+	
+
+}
