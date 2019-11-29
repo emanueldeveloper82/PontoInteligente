32cd41fac04eccc040fe8f70a524873fba1f1103
Emanuel da Anunciação Silva <emanuel.silva@solutis.com.br>
2019-11-29 15:36:55 -0300

    Dto e Test de empresa e seu método de busca por CNPJ concluído.

---
+++ src/main/java/br/com/eps/pontointeligente/api/controllers/EmpresaController.java
@@ -0,0 +1,71 @@
+package br.com.eps.pontointeligente.api.controllers;
+
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.eps.pontointeligente.api.dtos.EmpresaDto;
+import br.com.eps.pontointeligente.api.entity.Empresa;
+import br.com.eps.pontointeligente.api.response.Response;
+import br.com.eps.pontointeligente.api.services.EmpresaService;
+
+/**
+ * DTO Empresas.
+ * @author emanuel developer
+ * 
+ */
+@RestController
+@RequestMapping("/api/empresas")
+@CrossOrigin(origins = "*")
+public class EmpresaController {
+	
+	private static final Logger log = LoggerFactory.getLogger(CadastroPJController.class);
+	
+	@Autowired
+	private EmpresaService empresaService;
+	
+	public EmpresaController() {}
+	
+	/**
+	 * Método que busca uma empresa pelo CNPJ.
+	 * 
+	 * @param cnpj
+	 * @return ResponseEntity<Response<EmpresaDto>>
+	 */
+	@GetMapping("/cnpj/{cnpj}")
+	public ResponseEntity<Response<EmpresaDto>> buscarPorCnpj(@PathVariable("cnpj") String cnpj) {
+		
+		log.info("Buscando empresa pelo CNPJ: {}", cnpj);
+		
+		Response<EmpresaDto> response = new Response<>();
+		Optional<Empresa> empresa = this.empresaService.buscarPorNumCnpj(cnpj);
+
+		if (!empresa.isPresent()) {
+			log.info("Empresa não localizada pelo CNPJ: {}", cnpj);
+			response.getErrors().add("Empresa não localizada pelo CNPJ "+ cnpj);
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		response.setData(this.converterEmpresaParaDto(empresa.get()));
+		return ResponseEntity.ok(response);
+	}
+
+	/**
+	 * Método que converte uma empresa para DTO.
+	 * @param empresa
+	 * @return
+	 */
+	private EmpresaDto converterEmpresaParaDto(Empresa empresa) {
+		EmpresaDto empresaDto = new EmpresaDto();
+		empresaDto.setId(empresa.getIdEmpresa());
+		empresaDto.setCnpj(empresa.getNumCnpj());
+		empresaDto.setRazaoSocial(empresa.getRazaoSocial());
+		return empresaDto;
+	}
+}
