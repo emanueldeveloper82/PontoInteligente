70294923800705f245e5b773ccb55bd7a8cab24c
Emanuel <emanuel.silva@opah.com.br>
2021-10-07 21:11:56 -0300

    Adicionando modelmap ao init da aplicação;

--- src/main/java/br/com/eps/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/br/com/eps/pontointeligente/api/PontoInteligenteApplication.java
@@ -1,8 +1,10 @@
 package br.com.eps.pontointeligente.api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,6 +12,11 @@ public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontoInteligenteApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
